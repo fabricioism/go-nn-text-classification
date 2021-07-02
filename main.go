4058534a81379371ee7192c564455b4550b6e9c0
Fabricio Murillo <fabriciomurillo24@gmail.com>
2021-07-02 09:56:02 -0600

    :rocket: Cors

--- main.go
+++ main.go
@@ -4,16 +4,22 @@ import (
 	"net/http"
 
 	net "github.com/fabricioism/go-text-classification/net/processing"
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+
 	r.Get("/v1/bot", func(w http.ResponseWriter, r *http.Request) {
 		predict := net.Predict("quiero ordenar una pizza")
 		w.Write([]byte(predict))
 	})
-	http.ListenAndServe("0.0.0.0:3000", r)
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(r)
+	// http.ListenAndServe(":8080", handler)
+	http.ListenAndServe("0.0.0.0:3000", handler)
 }
