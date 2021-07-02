9432c61f93b69c61173a22193ff52a06df999e79
Fabricio Murillo <fabriciomurillo24@gmail.com>
2021-07-02 16:57:21 -0600

    :rocket: Add middlewares

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Mount("/v1/predictions", predictionsResource{}.Routes())
 	handler := cors.Default().Handler(r)
 
