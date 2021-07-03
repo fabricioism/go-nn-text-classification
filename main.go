8c1258360706be30b179e76ad876a5dfb4d922b5
Fabricio Murillo <fabriciomurillo24@gmail.com>
2021-07-02 20:53:46 -0600

    :hammer: Hot fix with classes

--- main.go
+++ main.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/rs/cors"
+	net "github.com/fabricioism/go-text-classification/net/processing"
 )
 
-func main() {
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	r.Mount("/v1/predictions", predictionsResource{}.Routes())
-	handler := cors.Default().Handler(r)
-
-	http.ListenAndServe("0.0.0.0:3000", handler)
-}
-
 // func main() {
-// 	err := net.TrainModel()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+// 	r := chi.NewRouter()
+
+// 	r.Use(middleware.RequestID)
+// 	r.Use(middleware.RealIP)
+// 	r.Use(middleware.Logger)
+// 	r.Use(middleware.Recoverer)
+
+// 	r.Mount("/v1/predictions", predictionsResource{}.Routes())
+// 	handler := cors.Default().Handler(r)
+
+// 	http.ListenAndServe("0.0.0.0:3000", handler)
 // }
+
+func main() {
+	err := net.TrainModel()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
